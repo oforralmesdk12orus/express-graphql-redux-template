c5aedd85287703955c6836cfb78b4e87689c2d59
Oliver Forral <oforral@mesd.k12.or.us>
2016-10-05 11:24:03 -0700

    Added redux code

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { queryReducer } from "./app/reducers/reducers.js";
+import thunkMiddleware from "redux-thunk";
+import { QueryContainer } from "./app/components/Query.js";
+
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware
+)(createStore)
+
+const Main = () => {
+  return (
+    <div>
+      <QueryContainer />
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(queryReducer)}>
+    <Main />
+  </Provider>,
+  document.getElementById("example")
+);
